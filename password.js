8c3bac5959207e30aa8d09fb325c8c0f0ff21eec
occohen <ori@adalon.com>
2016-11-30 17:40:36 -0800

    password
    no more button to webpage

--- password.js
+++
@@ -1,9 +0,0 @@
-function password (){
-	var passy = prompt("What is the password?");
-	while(passy !== "f527")
-	{
-		alert("WRONG enter the password again");
-		passy = prompt("What is the password?");
-	}
-	document.getElementById("pass").innerHTML += "Proceed to the next page";
-}
